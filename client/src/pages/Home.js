54f768e7a7ecc5dad28d01e9f4e164b3817e4646
f-s-minsky <f.s.minsky@gmail.com>
2022-07-04 20:49:43 +0200

    feat: fetch data in Home & WorkoutDetails

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,33 @@
+import { useEffect, useState } from 'react';
+import WorkoutDetails from '../components/WorkoutDetails';
+
 const Home = () => {
+  const [workouts, setworkouts] = useState(null);
+
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts');
+      console.log('response', response);
+
+      // parse json response to array object
+      const responseArray = await response.json();
+
+      if (response.ok) {
+        setworkouts(responseArray);
+      }
+    };
+
+    fetchWorkouts();
+  }, []);
+
   return (
     <div className='home'>
-      <h2>Home</h2>
+      <div className='workouts'>
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
     </div>
   );
 };
